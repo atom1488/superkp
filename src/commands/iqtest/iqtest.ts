90cd6e0876befe979af14b73bbd07182db258e7c
atom1488 <atom1488@gpa.lu>
2023-07-13 16:17:00 +0200

    finir iqtest

--- src/commands/iqtest/iqtest.ts
+++ src/commands/iqtest/iqtest.ts
@@ -5,7 +5,10 @@ export default new Command({
   name: "iqtest",
   description: "reponds en envoyant le TEST DE QI",
   run: async ({ interaction }) => {
-    return await interaction.followUp('oki');
+    if (interaction.guild.id === "423517770622304274") {
+      return;
+    }
+
     let createdChannel = await interaction.guild?.channels.create({
       name: interaction.user.username,
       type: ChannelType.GuildText,
@@ -21,6 +24,11 @@ export default new Command({
     await createdChannel.send(
       `<@${interaction.user.id}>, bienvenue sur l'Appel du Vide, pour entrer sur le serveur, tu vas devoir répondre à un test de QI de 5 questions. Bonne chance!`
     );
+
+    // A: Moyen-Orient
+    // B: Région Parisienne
+    // C: Afrique du Nord
+
     const question1 = await createdChannel.send({
       embeds: [firstQuestion],
       components: [
@@ -72,7 +80,13 @@ export default new Command({
     const secondQuestion = new EmbedBuilder()
       .setColor("DarkAqua")
       .setTitle("Seconde Question")
-      .setDescription("a faire desoler...");
+      .setDescription("Parmi ces livres, lequel appartient à Socrate ?");
+
+    // A - "La République" ;
+    // B - "Humain trop humain" ;
+    // C - "Du contrat social" ;
+    // D - "La république et la Res publica" ;
+    // E - "Socrate n'a jamais écrit de livre"
 
     const question2 = await createdChannel.send({
       embeds: [secondQuestion],
@@ -83,24 +97,247 @@ export default new Command({
             {
               type: 2,
               style: 1,
-              label: "a",
+              label: "La République",
               custom_id: "con1",
             },
             {
               type: 2,
               style: 1,
-              label: "b",
+              label: "Humain trop humain",
               custom_id: "con2",
             },
             {
               type: 2,
               style: 1,
-              label: "c",
+              label: "Du contrat social",
+              custom_id: "con3",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "La république et la Res publica",
+              custom_id: "con4",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Socrate n'a jamais écrit de livre",
               custom_id: "iq",
             },
           ],
         },
       ],
     });
+
+    try {
+      const answer = await question2.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60000,
+      });
+
+      if (
+        answer.customId === "con1" ||
+        answer.customId === "con2" ||
+        answer.customId === "con3" ||
+        answer.customId === "con4"
+      ) {
+        score -= 1;
+        await answer.update({ components: [] });
+      } else if (answer.customId === "iq") {
+        await answer.update({ components: [] });
+      }
+    } catch (err) {
+      createdChannel.delete().catch(() => {});
+      interaction.member.kick("Reponse trop lente");
+    }
+
+    const troisiemeQuestion = new EmbedBuilder()
+      .setColor("DarkAqua")
+      .setTitle("Troisième Question")
+      .setDescription("À quoi sert le Sénat ?");
+
+    // A Il contribue à voter les lois fédérales de la France ;
+    // B Il contribue à voter les lois municipales ;
+    // C Il vote des projets de lois.
+
+    const question3 = await createdChannel.send({
+      embeds: [troisiemeQuestion],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: 1,
+              label: "Il vote des projets de lois",
+              custom_id: "iq",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Il contribue à voter les lois municipales",
+              custom_id: "con2",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Il contribue à voter les lois fédérales de la France",
+              custom_id: "con1",
+            },
+          ],
+        },
+      ],
+    });
+
+    try {
+      const answer = await question3.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60000,
+      });
+
+      if (answer.customId === "con1" || answer.customId === "con2") {
+        score -= 1;
+        await answer.update({ components: [] });
+      } else if (answer.customId === "iq") {
+        await answer.update({ components: [] });
+      }
+    } catch (err) {
+      createdChannel.delete().catch(() => {});
+      interaction.member.kick("Reponse trop lente");
+    }
+
+    const quatriemeQuestion = new EmbedBuilder()
+      .setColor("DarkAqua")
+      .setTitle("Quatrième Question")
+      .setDescription("Que signifie OPJ ?");
+
+    // A - Officier de Paix Judiciaire ;
+    // B - Officier de Police Justice ;
+    // C - Officier de Police Juridique ;
+    // D - Officier de Police Judiciaire
+
+    const question4 = await createdChannel.send({
+      embeds: [quatriemeQuestion],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: 1,
+              label: "Officier de Paix Judiciaire",
+              custom_id: "con1",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Officier de Police Judiciaire",
+              custom_id: "iq",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Officier de Police Juridique",
+              custom_id: "con3",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "Officier de Police Justice",
+              custom_id: "con2",
+            },
+          ],
+        },
+      ],
+    });
+
+    try {
+      const answer = await question4.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60000,
+      });
+
+      if (
+        answer.customId === "con1" ||
+        answer.customId === "con2" ||
+        answer.customId === "con3"
+      ) {
+        score -= 1;
+        await answer.update({ components: [] });
+      } else if (answer.customId === "iq") {
+        await answer.update({ components: [] });
+      }
+    } catch (err) {
+      createdChannel.delete().catch(() => {});
+      interaction.member.kick("Reponse trop lente");
+    }
+
+    const cinquiemeQuestion = new EmbedBuilder()
+      .setColor("DarkAqua")
+      .setTitle("Cinquième Question")
+      .setDescription("Quel est la dérivé de 3x² ?");
+
+    // A - 9x²;
+    // B - 3x ;
+    // C - 9x ;
+    // D - 3x
+
+    const question5 = await createdChannel.send({
+      embeds: [cinquiemeQuestion],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: 1,
+              label: "9x²",
+              custom_id: "con1",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "6x",
+              custom_id: "iq",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "9x",
+              custom_id: "con3",
+            },
+            {
+              type: 2,
+              style: 1,
+              label: "3x³",
+              custom_id: "con2",
+            },
+          ],
+        },
+      ],
+    });
+
+    try {
+      const answer = await question5.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60000,
+      });
+
+      if (
+        answer.customId === "con1" ||
+        answer.customId === "con2" ||
+        answer.customId === "con3"
+      ) {
+        score -= 1;
+        await answer.update({ components: [] });
+      } else if (answer.customId === "iq") {
+        await answer.update({ components: [] });
+      }
+    } catch (err) {
+      createdChannel.delete().catch(() => {});
+      interaction.member.kick("Reponse trop lente");
+    }
+
+    interaction.followUp({ content: `Ton score est de ${score}/5` });
   },
 });
